1473cda6f3bce796fc76edee0cfec3a38a987dbc
NNSSPPP <168963651+NNSSPPP@users.noreply.github.com>
2025-08-20 16:26:23 +0700

    fix error dashboard page partnotification and import comonModule

--- codereviewFE/src/app/dashboard/dashboard.component.ts
+++ codereviewFE/src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-  constructor(private router: Router) { }
+  constructor(private readonly router: Router) { }
 
   notifications = 3;
 
