5963e8be88b46d371248a2f7578d0b4ccca7a21f
NNSSPPP <168963651+NNSSPPP@users.noreply.github.com>
2025-09-03 15:29:38 +0700

    created analytics overview page

---
+++ codereviewFE/src/app/securitydashboard/securitydashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-securitydashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './securitydashboard.component.html',
+  styleUrl: './securitydashboard.component.css'
+})
+export class SecuritydashboardComponent {
+
+}
---
+++ codereviewFE/src/app/technicaldebt/technicaldebt.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-technicaldebt',
+  standalone: true,
+  imports: [],
+  templateUrl: './technicaldebt.component.html',
+  styleUrl: './technicaldebt.component.css'
+})
+export class TechnicaldebtComponent {
+
+}
