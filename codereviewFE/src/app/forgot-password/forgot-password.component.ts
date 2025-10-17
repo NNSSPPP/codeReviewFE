4f87457a7e7f10326f40c529764240e4d7ac328f
Parichart Sukkarat <parichartsukkarat@MacBook-Air-khxng-Parichart.local>
2025-10-17 09:41:50 +0700

    up date css forgotpassword

--- codereviewFE/src/app/forgot-password/forgot-password.component.ts
+++ codereviewFE/src/app/forgot-password/forgot-password.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { FormBuilder, Validators, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../services/authservice/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-forgot-password',
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './forgot-password.html',
+  styleUrls: ['./forgot-password.scss'],
 })
 export class ForgotPasswordComponent {
   form: FormGroup;
@@ -16,9 +18,9 @@ export class ForgotPasswordComponent {
 
   constructor(
     private fb: FormBuilder,
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router
   ) {
-    // ✅ กำหนดฟอร์มใน constructor
     this.form = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
     });
@@ -32,17 +34,21 @@ export class ForgotPasswordComponent {
 
     const email = this.form.get('email')?.value;
 
-    this.authService.request(email).subscribe({
-      next: (res) => {
-        // ✅ backend ส่งข้อความอะไรมาก็เอามาแสดง หรือใช้ข้อความ default
-        this.msg = res?.message || 'ถ้ามีบัญชีนี้ เราได้ส่งลิงก์รีเซ็ตไปที่อีเมลแล้ว';
-        this.loading = false;
-        this.form.reset();
-      },
-      error: (err) => {
-        this.msg = err.error?.message || 'เกิดข้อผิดพลาด กรุณาลองใหม่';
-        this.loading = false;
-      },
-    });
+    // this.authService.request(email).subscribe({
+    //   next: (res) => {
+    //     this.msg = res?.message || 'ถ้ามีบัญชีนี้ เราได้ส่งลิงก์รีเซ็ตไปที่อีเมลแล้ว';
+    //     this.loading = false;
+    //     this.form.reset();
+    //   },
+    //   error: (err) => {
+    //     this.msg = err.error?.message || 'เกิดข้อผิดพลาด กรุณาลองใหม่';
+    //     this.loading = false;
+    //   },
+    // });
   }
+
+  goBack(): void {
+    this.router.navigate(['/login']);
+  }
+  
 }
